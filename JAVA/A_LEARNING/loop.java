03d24f623562d6cf9e41abe25bf7ae4838869aed
JAS2343L <js9404107@gmail.com>
2026-08-12 21:24:14 +0530

    Added all old java learnings into an .zip file

--- JAVA/A_LEARNING/loop.java
+++
@@ -1,8 +0,0 @@
-package mypersonal;
-public class loop {
-    public static void main(String[] args) {
-        for(int a = 1; a <= 1000000; a++){
-            System.out.println(a);
-        }
-    }
-}
